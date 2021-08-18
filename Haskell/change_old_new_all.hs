5f640c2e9743b6e0917af5ef8be35315b851a812
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2021-08-17 23:02:48 -0300

    identacao codigo change all

--- Haskell/change_old_new_all.hs
+++ Haskell/change_old_new_all.hs
@@ -1,2 +1,4 @@
 change_old_new_all _ _ [] = []
-change_old_new_all old new (x:xs) = if x == old then [new] ++ change_old_new_all old new xs else [x] ++ change_old_new_all old new xs
+change_old_new_all old new (x:xs) = if x == old 
+                                    then [new] ++ change_old_new_all old new xs 
+                                    else [x] ++ change_old_new_all old new xs
