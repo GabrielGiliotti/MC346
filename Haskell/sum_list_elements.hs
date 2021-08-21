2ae928e9a320ff277aca41ac573b981821511e71
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2021-08-20 23:11:04 -0300

    soma lista com definição do tipo de função e acumulador

--- Haskell/sum_list_elements.hs
+++ Haskell/sum_list_elements.hs
@@ -1,2 +1,7 @@
+sum_elements :: Num t => [t] -> t
 sum_elements [] = 0
-sum_elements (x:xs) = x + sum_elements xs
+sum_elements (x:xs) = sum_elements_acc (x:xs) 0
+  where
+    sum_elements_acc :: Num v => [v] -> v -> v
+    sum_elements_acc [] acc = acc
+    sum_elements_acc (x:xs) acc = sum_elements_acc xs (x+acc)
