e4408f9e60e0223322b786ea2d9ffa45d37bdebd
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2021-08-20 23:49:02 -0300

    definição da função para change old new once

--- Haskell/change_old_new_once.hs
+++ Haskell/change_old_new_once.hs
@@ -1,2 +1,5 @@
+change_old_new_once :: Eq t => t -> t -> [t] -> [t]
 change_old_new_once _ _ [] = []
-change_old_new_once old new (x:xs) = if x == old then [new] ++ xs else [x] ++ change_old_new_once old new xs
+change_old_new_once old new (x:xs) 
+  | x == old = [new] ++ xs 
+  | otherwise = [x] ++ change_old_new_once old new xs
