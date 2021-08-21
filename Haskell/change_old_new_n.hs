54c4526379771269cec509bd6aa6f423f0b7fb0e
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2021-08-21 00:21:38 -0300

    change old new n times melhorado

--- Haskell/change_old_new_n.hs
+++ Haskell/change_old_new_n.hs
@@ -1,5 +1,5 @@
-change_old_new_once _ _ [] = []
-change_old_new_once old new (x:xs) = if x == old then [new] ++ xs else [x] ++ change_old_new_once old new xs
-
+change_old_new_n :: (Ord a, Num a) => a -> a -> a -> [a] -> [a]
 change_old_new_n _ _ _ [] = []
-change_old_new_n old new n (x:xs) = if n > 0 then change_old_new_n old new (n-1) (change_old_new_once old new (x:xs)) else (x:xs)
+change_old_new_n old new n (x:xs) 
+  | (n > 0) && (x == old) = new : change_old_new_n old new (n-1) xs
+  | otherwise = x : change_old_new_n old new n xs
