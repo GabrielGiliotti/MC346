7e89b14a3c12da301dc811ee3387a75a09f7358b
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2021-09-09 22:36:05 -0300

    descomprime usando replicate

--- Haskell/compression.hs
+++ Haskell/compression.hs
@@ -1,5 +1,6 @@
 import Data.List (groupBy)
 
+-- comprime
 -- agrupa ocorrencias sequenciais em diferentes listas
 agrupa_iguais :: (Eq a) => [a] -> [[a]]
 agrupa_iguais [] = []
@@ -11,4 +12,6 @@ comprime :: (Eq a) => [a] -> [(a,Int)]
 comprime xs = map (\(e:list) -> (e, length (e:list))) (agrupa_iguais xs)
 
 
-
+-- descomprime
+descomprime :: (Eq a) => [(a,Int)] -> [a]
+descomprime xs = [z | (x,y) <- xs, z <- (replicate y x)]
