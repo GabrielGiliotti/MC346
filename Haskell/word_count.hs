3b303a18cf6ec1364b619d881332c4e39f923abd
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2021-09-15 19:07:20 -0300

    para submeter

--- Haskell/word_count.hs
+++ Haskell/word_count.hs
@@ -2,7 +2,7 @@ import Data.Char
 import System.IO
 
 count_letters_all_words :: String -> String
-count_letters_all_words string = string ++ " - " ++ show (length (string)) ++ " letra(s)\n"
+count_letters_all_words string = string ++ " - " ++ show (length (string)) ++ " caracter(es)\n"
 
 iterate_list :: [String] -> String
 iterate_list [] = []
@@ -13,6 +13,6 @@ main = do
     hSetEncoding stdin utf8 --Precisei setar para utf8 para pegar caracteres especiais
     contents <- getContents
     putStrLn ("\nSaida:\nPalavras: " ++ show (length (words contents)))
-    putStrLn ("\n---------------------\n")
+    putStrLn ("\n---------------------------------\n")
     putStrLn (iterate_list (words (contents)))
-    putStrLn ("---------------------\n\nTotal de Caracteres: " ++ show (length (concat (words contents))))
+    putStrLn ("---------------------------------\n\nTotal de Caracteres: " ++ show (length (concat (words contents))))
