c05f7efe1076e5db800a23a8a97efb2df6b36930
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2021-09-15 18:48:11 -0300

    remoção função inutil

--- Haskell/word_count.hs
+++ Haskell/word_count.hs
@@ -1,9 +1,6 @@
 import Data.Char  
 import System.IO
 
-count_words :: String -> Int
-count_words input = length(words(input))
-
 count_letters_all_words :: String -> String
 count_letters_all_words string = string ++ ": " ++ show (length (string)) ++ "\n"
 
@@ -15,7 +12,7 @@ main :: IO ()
 main = do  
     hSetEncoding stdin utf8 --Precisei setar para utf8 para pegar caracteres especiais
     contents <- getContents
-    putStrLn ("\nSaida:\nPalavras: " ++ show (count_words contents))
+    putStrLn ("\nSaida:\nPalavras: " ++ show (length (words contents)))
     putStrLn ("\n--------------\n")
     putStrLn (iterate_list (words (contents)))
     putStrLn ("--------------\n\nTotal de Caracteres: " ++ show (length (concat (words contents))))
